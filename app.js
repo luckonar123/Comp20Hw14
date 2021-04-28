eb7dd353470c9a671d4cf2b1bceb32944e874b8f
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 02:18:31 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,86 +1,89 @@
-//get each line from .cvs file
-var readline = require('readline');
-var fs = require('fs');
-
-var companiesToTickers = {}
-var tickersToCompanies = {}
-
-
-//connect to mongo 
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority"
-
-
-MongoClient.connect(url, function(err, db) {
-  if(err) { return console.dir(err); }
-  var dbo = db.db("hw14c");
-  var query = {};
-  dbo.collection("companies").find(query).toArray(function(err, result) {
-    if (err) throw err;
-    // console.log(result);
-    for (i = 0; i < result.length; i++) {
-      companiesToTickers[result[i].Company] = result[i].Ticker;
-      if (tickersToCompanies[result[i].Ticker] == undefined) {
-        tickersToCompanies[result[i].Ticker] = "";
-      } else {
-        tickersToCompanies[result[i].Ticker] += ", ";
-      }
-      tickersToCompanies[result[i].Ticker] += result[i].Company;
-      console.log(result[i].Ticker + " " + result[i].Company);
-    }
-    db.close();
-  });
-});
-
-connect to heroku
 var http = require('http');
+var url = require('url');
 var port = process.env.PORT || 3000;
-http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("hello");
-  res.end();
+
+const MongoClient = require('mongodb').MongoClient;
+const db_url = "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority";
+
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    var qobj = url.parse(req.url, true).query;
+    var type = qobj.type; // assume x is querystring parameter
+    var input = qobj.input;
+    var query = new Object();
+    query[type] = input;
+    res.write("<h1>Stock Ticker App</h1>");
+    res.write("<h3>" + type + ": " + input + "</h3>");
+
+    res.write("<h3> result" + ": " + " </h3>");
+
+    find(type, input, query).then(result => {
+        console.log(result);
+        res.end( result  );
+    });
+
 }).listen(port);
 
-var express = require('express');
-var app = express();
-app.get('/',function(req,res){
-    res.sendFile('index.html', {root : __dirname});
-})
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/',function(req,res){
-    var companyInput = req.body.company;
-    var tickerInput = req.body.ticker;
-    
-    if(companyInput == "" && tickerInput == ""){
-      return;
+function createOption(type) {
+    var answer = {
+        "_id": 0,
+        "Company": 0,
+        "Ticker": 1
+    };
+    if (type == "Ticker") {
+        return {
+            "_id": 0,
+            "Company": 1,
+            "Ticker": 0
+        };
     }
-    if(companyInput != "" && tickerInput != ""){
-      return;
-    }
-    
-    //get ticker name
-    if(companyInput == ""){
-      companyResult = tickersToCompanies[tickerInput];
-      // res.json({callback: companyResult});
-      res.send(companyResult)
-      // res.sendFile('index.html', {root : __dirname});
-      return;
-    }
-    //get company name
-    if(tickerInput == ""){
-      tickerResult = companiesToTickers[companyInput];
-      // res.json({callback: companyResult});
-      res.send(tickerResult)
-      // res.sendFile('index.html', {root : __dirname});
-      return;
-    }
-    
-});
+    return answer;
+}
 
-app.listen(8080, function() {
-  console.log('Server running at http://127.0.0.1:8080/');
-});
+
+
+async function find(type, input, query) {
+    try {
+        client = new MongoClient(db_url, {
+            useUnifiedTopology: true
+        });
+        await client.connect();
+        var dbo = client.db("Hw14");
+        var coll = dbo.collection("companies");
+        var options = createOption(type);
+        var promise;
+
+        var results = "";
+        var t1 = "Ticker";
+        if (type == "Ticker") {
+            t1 = "Company";
+        }
+        var answer = "";
+        console.log(query);
+        await coll.find(query, options).toArray().then(answer => {
+            promise = new Promise(function(resolve, reject) {
+                setTimeout(function() {
+                    i = 1;
+                    answer.forEach(function(item) {
+                        results += "<h4>"+ t1 + " " + i + ": " + item[t1] + "\n" + "</h4>";
+                        i++;
+                    });
+                    resolve(results );
+                });
+            });
+        }).catch(err => console.log(err))
+        return promise;
+
+    } catch (err) {
+        console.log("Database error: " + err);
+    } finally {
+
+        client.close();
+    }
+
+}
