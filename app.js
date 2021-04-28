0054b7a1a1106e4dcc69b0a08e24c8f7795b4a7e
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 02:38:56 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
 var http = require('http');
-var url = require('url');
 var port = process.env.PORT || 3000;
+http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello my name is Lcuas <br>');
 
-const MongoClient = require('mongodb').MongoClient;
-const db_url = "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority";
-
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    var qobj = url.parse(req.url, true).query;
-    var type = qobj.type; // assume x is querystring parameter
-    var input = qobj.input;
-    var query = new Object();
-    query[type] = input;
-    res.write("<h1>Stock Ticker App</h1>");
-    res.write("<h3>" + type + ": " + input + "</h3>");
-
-    res.write("<h3> result" + ": " + " </h3>");
-
-    find(type, input, query).then(result => {
-        console.log(result);
-        res.end( result  );
-    });
-
+    res.end();
 }).listen(port);
