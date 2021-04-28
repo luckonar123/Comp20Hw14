3352a7e4024a6f516d645645a76c94a7cf1e8e41
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 01:58:46 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority";
-MongoClient.connect(url,function(err, db){
-  if(err){return console.log(err); return;}
-  var dbo = db.db("Hw14");
-  var collection = dbo.collection('companies');
-  db.close();
-});
+const url = 
+"mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority";
 
+ MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+      if(err) { return console.log(err); return;}  
+        var dbo = db.db("Hw14");
+        var collection = dbo.collection('companies');
+        console.log("Success!");
+        db.close();
+    });
+    
 
+    
+    
 
 var http = require('http');
 var port = process.env.PORT || 3000;
