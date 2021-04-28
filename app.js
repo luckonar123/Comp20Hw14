300705d19476ee5952c1d36468dce8ecef1fdeea
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 00:02:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello my name is Lcuas');
-    res.write('I live in asstown');
-    res.write('dick');
+    res.write('Hello my name is Lcuas <br>');
 
     res.end();
 }).listen(port);
 
 
-app.use(express.urlencoded());
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+    res.sendFile('index.html', {root : __dirname});
+})
 
-app.use(express.json());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', function(request, response){
-    console.log(request.body.ticker);
-    console.log(request.body.button);
-});
-
-res.write(body.ticker);
+app.post('/',function(req,res){
+    var companyInput = req.body.company;
+    var tickerInput = req.body.ticker;
