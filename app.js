da677ac2d41fd82eda927e289738df5fe8317b34
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 16:25:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const server = http.createServer((req, res) => {
     var contentType = getContType(filePath);
     // console.log(contentType);
 
-    if (req.url == '/result') {
+    if (req.url == '') {
         res.writeHead(200, {'Content-Type': 'text/html'}); 
         // res.write ("Process the POST request<br>"); 
         pdata = ""; 
