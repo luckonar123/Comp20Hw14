7bc42af088405524ac07d16af5c4a2ef9579efa7
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 02:43:13 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var url = require('url');
+
 var port = process.env.PORT || 3000;
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello my name is Lcuas <br>');
-
+    var qobj = url.parse(req.url, ture).query;
+    var txt = qobj.id;
+    res.write("the value is: " + txt);
     res.end();
 }).listen(port);
