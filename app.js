3a97185fbefc7cab59c0eb11133ebc457c712f68
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 02:09:23 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello my name is Lcuas <br>');
+
+    res.end();
+}).listen(port);
+
+
+
 const MongoClient = require('mongodb').MongoClient;
 const url =     "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority"
 
@@ -22,13 +33,3 @@ MongoClient.connect(url, function(err, db) {
     db.close();
   });
 });
-    
-
-var http = require('http');
-var port = process.env.PORT || 3000;
-http.createServer(function (req,res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello my name is Lcuas <br>');
-
-    res.end();
-}).listen(port);
