e5541e0b780bcc03d868a0a6434e732f628fdc15
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 02:44:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var url = require('url');
 var port = process.env.PORT || 3000;
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var qobj = url.parse(req.url, ture).query;
+    var qobj = url.parse(req.url, true).query;
     var txt = qobj.id;
     res.write("the value is: " + txt);
     res.end();
