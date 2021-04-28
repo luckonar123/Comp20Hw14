8d33f4971752bd2a993e8ac2bcfbfb70b3d298c3
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-27 23:29:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
 http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hello my name is Lcuas');
@@ -6,4 +7,4 @@ http.createServer(function (req,res) {
     res.write('dick');
 
     res.end();
-}).listen(8080);
+}).listen(port);
