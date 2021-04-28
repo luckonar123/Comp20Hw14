48a70bb1c2c6d52b4cb5acfa97e99891f3e69349
luckonar123 <78707429+luckonar123@users.noreply.github.com>
2021-04-28 02:16:17 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var http = require('http');
-var port = process.env.PORT || 3000;
-http.createServer(function (req,res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello my name is Lcuas <br>');
+//get each line from .cvs file
+var readline = require('readline');
+var fs = require('fs');
 
-    res.end();
-}).listen(port);
+var companiesToTickers = {}
+var tickersToCompanies = {}
 
 
-
+//connect to mongo 
 const MongoClient = require('mongodb').MongoClient;
-const url =     "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority"
+const url = "mongodb+srv://luckonar:Luckonar123@cluster0.7agxc.mongodb.net/Hw14?retryWrites=true&w=majority"
 
 
 MongoClient.connect(url, function(err, db) {
   if(err) { return console.dir(err); }
-  var dbo = db.db("Hw14");
+  var dbo = db.db("hw14c");
   var query = {};
   dbo.collection("companies").find(query).toArray(function(err, result) {
     if (err) throw err;
@@ -32,4 +30,57 @@ MongoClient.connect(url, function(err, db) {
     }
     db.close();
   });
+});
+
+connect to heroku
+var http = require('http');
+var port = process.env.PORT || 3000;
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("hello");
+  res.end();
+}).listen(port);
+
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+    res.sendFile('index.html', {root : __dirname});
+})
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/',function(req,res){
+    var companyInput = req.body.company;
+    var tickerInput = req.body.ticker;
+    
+    if(companyInput == "" && tickerInput == ""){
+      return;
+    }
+    if(companyInput != "" && tickerInput != ""){
+      return;
+    }
+    
+    //get ticker name
+    if(companyInput == ""){
+      companyResult = tickersToCompanies[tickerInput];
+      // res.json({callback: companyResult});
+      res.send(companyResult)
+      // res.sendFile('index.html', {root : __dirname});
+      return;
+    }
+    //get company name
+    if(tickerInput == ""){
+      tickerResult = companiesToTickers[companyInput];
+      // res.json({callback: companyResult});
+      res.send(tickerResult)
+      // res.sendFile('index.html', {root : __dirname});
+      return;
+    }
+    
+});
+
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
 });
